68f4c681b80019550db3db110c21a7e851383897
Ritesh2811 <ricksrv.5@gmail.com>
2019-02-01 14:08:26 +0530

    added the hash functionality and verification

--- blockChainCore.py
+++ blockChainCore.py
@@ -1,7 +1,67 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+# adding the core of the blockChain
+# step one creating the block
+## later we will convert it to currency
 
-This is a temporary script file.
-"""
 
+
+import datetime
+import hashlib
+import json
+
+from flask import Flask, jsonify
+
+
+# building a blockchain
+
+class coreChain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, prev_hash ='0')
+    
+    def create_block (self,proof,prev_hash):
+        block = {'index':len(self.chain+1),
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'prev_hash': prev_hash
+                 }
+        self.chain.append(block)
+        return block
+        
+    
+    def get_prev_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, prev_proof):
+        new_proof  = 1
+        check_proof = False
+        while(check_proof  is False):
+            hash_operation =  hashlib.sha256(str(new_proof**2 - prev_proof**2 ).encode()).hexdigest()
+            
+            if hash_operation[:6] =='000000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    def hash(self,block):
+        encoded_block = json.dumps(block,sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self,chain):
+        prev_block = chain[0]
+        block_index = 1
+        
+        while block_index  < len(chain):
+            block = chain[block_index]
+            if block['prev_hash'] != self.hash(prev_block):
+                return False
+            prev_proof = prev_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_operation[:6] !='000000':
+                return False
+            prev_block = block
+            block += 1
+                
+                
